b4e25c1507d8608b0b0b97ea614d9d5af7126f92
Salih Alborno <salih.alborno@gmail.com>
2017-06-15 22:29:36 +0100

    we have a map with user location tracking first 20 secs and zoom in location campus

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -7,13 +7,51 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    
+    var updateCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print ("Ready to Go Pokemon")
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+        } else {
+            manager.requestWhenInUseAuthorization()   //.requestAlwaysAuthorization() even when app is closed, can cause mistrust with users, dains battery. Check privacy settings to match.
+        }
+        
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print ("We made it: \(updateCount)")
+        if updateCount < 20 {
+            let region = MKCoordinateRegionMakeWithDistance((manager.location?.coordinate)!, 400, 400)
+            mapView.setRegion(region, animated: false) //animated: true, causes map fuzzy start, if you want!
+            updateCount += 1
+        } else {
+            manager.stopUpdatingLocation() //to save battery life
+        }
+    }
+    
+    
+    @IBAction func centerTapped(_ sender: Any) {
+        if let coord = manager.location?.coordinate { //to stop app crashing when updating location
+            let region = MKCoordinateRegionMakeWithDistance(coord, 400, 400)
+            mapView.setRegion(region, animated: true)
+        }
+
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
